3f1c03b12ca1bf7ccd59df5b65502d32bbdde10a
Monika Dangol <moneeka.dangol1@gmail.com>
2025-09-07 22:47:24 +0545

    feat: add images and implement banner section with swiper functionality

---
+++ js/main.js
@@ -0,0 +1,12 @@
+
+   const swiper = new Swiper(".mySwiper", {
+      effect: "fade",              
+      fadeEffect: { crossFade: true },
+      loop: true,
+      autoplay: {
+        delay: 5000,                
+        disableOnInteraction: false,
+      },
+    
+    });
+
